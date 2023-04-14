06c5aa9be8a74f689ebbb4e45a5ae3b81a222d7e
Madina192 <madinaakpaeva000@gmail.com>
2023-04-14 17:57:10 +0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> names_A = new ArrayList<>();
+        ArrayList<String> names_B = new ArrayList<>();
+
+        System.out.println("Enter names for list A:");
+        for (int i = 0; i < 5; i++) {
+            names_A.add(scanner.nextLine());
+        }
+
+        System.out.println("names A: " + names_A);
+
+        System.out.println("Enter names for list B:");
+        for (int i = 0; i < 5; i++) {
+            names_B.add(scanner.nextLine());
+        }
+        System.out.println("names B: " + names_B);
+
+        ArrayList<String> names_C = new ArrayList<>();
+
+        int indexForA = 0;
+        int indexForB = 1;
+        Collections.reverse(names_B);
+
+        for (int i = 0; i < names_A.size(); i++) {
+            names_C.add(indexForA, names_A.get(i));
+            names_C.add(indexForB, names_B.get(i));
+            indexForA+=2;
+            indexForB+=2;
+        }
+        System.out.println("names C: " + names_C);
+
+        names_C.sort(Comparator.comparingInt(String::length));
+
+        System.out.println("Sorted names C: " + names_C);
+    }
+}
